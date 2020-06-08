acf9222219071f90cd42dd562ceadfb7b41dc7d9
Walid Amriou <walidamriou@gmail.com>
2020-06-08 03:46:35 +0200

    Update server/main.go test http webserver

--- server/main.go
+++ server/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
-	"github.com/go-vgo/robotgo"
+	"fmt"
+	"net/http"
 )
 
 func main() {
+	//http server
+	http.HandleFunc("/", http_Server)
+	http.ListenAndServe(":8080", nil)
+
 	//example of change the lang
-	robotgo.KeyTap("shift", "alt")
+	//robotgo.KeyTap("shift", "alt")
+}
+
+func http_Server(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 }
