7d4c9bbeab72f3b631e9b6752d7768cfb575b876
Walid Amriou <walidamriou@gmail.com>
2020-06-08 16:47:40 +0200

    Update add support get and post requestes

--- server/main2.go
+++ server/main2.go
@@ -1,3 +1,11 @@
+/*
+Moskotcho Slides Remote
+Remote control slides presentations From Web browsers & any device support WiFi
+----------
+by Walid Amriou
+https://moskotchosr.walidamriou.com
+---------
+*/
 package main
 
 import (
@@ -7,65 +15,71 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/bmizerany/pat"
 	"github.com/go-vgo/robotgo"
 )
 
-func http_Server(w http.ResponseWriter, r *http.Request) {
+func main() {
+	//To read css and js files from server
+	fs := http.FileServer(http.Dir("Templates"))
+	http.Handle("/Templates/", http.StripPrefix("/Templates/", fs))
 
-	t, err := template.ParseFiles("Templates/index.html") //parse the html file homepage.html
-	if err != nil {                                       // if there is an error
-		log.Print("template parsing error: ", err) // log it
-	}
-	err = t.Execute(w, nil) //execute the template and pass it the HomePageVars struct to fill in the gaps
-	if err != nil {         // if there is an error
-		log.Print("template executing error: ", err) //log it
-	}
+	//server
+	mux := pat.New()
+	//if user login to 192.168.1.4:8000 from browser, will see home html page
+	mux.Get("/", http.HandlerFunc(home))
+	//if user send a post request
+	mux.Post("/", http.HandlerFunc(send))
 
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-
-	switch r.Method {
-
-	case "GET":
-		for k, v := range r.URL.Query() {
-			fmt.Printf("%s: %s\n", k, v)
-		}
-		w.Write([]byte("Received a GET request\n"))
-
-	case "POST":
-		reqBody, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("%s\n", reqBody)
-		//convert reqBody from byte[] to string
-		s := string(reqBody[:])
-
-		//Test conditions with the data received
-		if s == "data=data1" {
-			fmt.Printf("The data is data1\n")
-		}
-
-		if s == "button=next" {
-			//robotgo.KeyTap("shift", "alt")
-			robotgo.KeyTap("right")
-		} else if s == "button=back" {
-			robotgo.KeyTap("left")
-		}
-		w.Write([]byte("Received a POST request\n"))
-
-	default:
-		w.WriteHeader(http.StatusNotImplemented)
-		w.Write([]byte(http.StatusText(http.StatusNotImplemented)))
-
+	//run the server
+	log.Println("Listening...")
+	err := http.ListenAndServe(":8000", mux)
+	// if there is an error
+	if err != nil {
+		// log the error
+		log.Fatal(err)
 	}
 
 }
 
-func main() {
-	http.HandleFunc("/", http_Server)
-	http.ListenAndServe(":8000", nil)
+func home(w http.ResponseWriter, r *http.Request) {
+	//parse the html file index.html
+	t, err := template.ParseFiles("Templates/index.html")
+	// if there is an error
+	if err != nil {
+		// log the error
+		log.Print("template parsing error: ", err)
+	}
+	//execute the template and pass it the HomePageVars struct to fill in the gaps
+	err = t.Execute(w, nil)
+	// if there is an error
+	if err != nil {
+		// log the error
+		log.Print("template executing error: ", err)
+	}
+}
+
+func send(w http.ResponseWriter, r *http.Request) {
+	//Read
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s\n", reqBody)
+	//convert reqBody from byte[] to string
+	s := string(reqBody[:])
+
+	//Test conditions with the data received
+	if s == "data=data1" {
+		fmt.Printf("The data is data1\n")
+	}
+
+	if s == "button=next" {
+		//robotgo.KeyTap("shift", "alt")
+		robotgo.KeyTap("right")
+	} else if s == "button=back" {
+		robotgo.KeyTap("left")
+	}
+	w.Write([]byte("Received a POST request\n"))
 }
